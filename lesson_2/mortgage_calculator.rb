1430e4f98565b216dfc288147ee105202d513606
Tim Brice <tim.j.brice@gmailcom>
2019-04-14 13:17:36 -0500

    Change messages to use .yml file

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
 def monthly_interest(annual_percentage_rate)
   annual_percentage_rate.to_f / 12
 end
@@ -6,7 +9,8 @@ def loan_duration_in_months(loan_duration)
   loan_duration.to_f / 12
 end
 
-def calculate_monthly_payment(total_amount, annual_percentage_rate, loan_duration)
+def calculate_monthly_payment(total_amount, annual_percentage_rate,
+                              loan_duration)
   j = monthly_interest(annual_percentage_rate)
   n = loan_duration_in_months(loan_duration)
 
@@ -20,58 +24,58 @@ def valid_number?(number)
   number.to_i.to_s == number
 end
 
-def prompt(message)
-  puts ">> #{message}"
+def messages(message)
+  ">> #{MESSAGES[message]}"
 end
 
-prompt("Welcome to the mortgage calculator.")
-prompt("We will calculate what your monthly payment will be.")
-prompt("To start, enter your name:")
+puts messages('welcome')
+puts messages('enter_name')
 
 name = ''
 loop do
   name = gets.chomp
 
   if name.empty?
-    prompt("Please enter your name.")
+    puts messages('invalid_name')
   else
     break
   end
 end
 
-prompt("Hi #{name}. Let's get some info to calculate your payment")
+puts format(messages('user_input'), name: 'Tim')
 
 total_amount = ''
 loop do
-  prompt("Please enter the total loan amount:")
+  puts messages('total_amount')
   total_amount = gets.chomp
   if valid_number?(total_amount)
     break
   else
-    prompt("Please enter a valid number for the total loan amount")
+    puts messages('invalid_total_amount')
   end
 end
 
 annual_percentage_rate = ''
 loop do
-  prompt("Please enter the annual percentage rate")
+  puts messages('annual_percentage_rate')
   annual_percentage_rate = gets.chomp
   if valid_number?(annual_percentage_rate)
     break
   else
-    prompt("Please enter a valid number for the annual percentage rate")
+    puts messages('invalid_annual_percentage_rate')
   end
 end
 
 loan_duration = ''
 loop do
-  prompt("Please enter the duration of that loan")
+  puts messages('total_duration')
   loan_duration = gets.chomp
   if valid_number?(loan_duration)
     break
   else
-    prompt("Please enter a valid loan duration")
+    puts messages('invalid_total_duration')
   end
 end
 
-puts "Your monthly payment will be #{calculate_monthly_payment(total_amount, annual_percentage_rate, loan_duration)}"
+monthly_payment = calculate_monthly_payment(total_amount, annual_percentage_rate, loan_duration)
+puts format(messages('monthly_payment'), payment: monthly_payment)
