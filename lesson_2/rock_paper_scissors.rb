eac99193c3b746a29c2a9b9593670ad855d9c2cc
Tim Brice <tim.j.brice@gmailcom>
2019-04-20 19:14:44 -0500

    Add logic for keeping score and determining winner

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -28,35 +28,71 @@ def win?(first, second)
   game_outcomes[first.to_sym].include?(second)
 end
 
-def display_result(player, computer)
+def get_result(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    'player'
   elsif win?(computer, player)
+    'computer'
+  else
+    'tie'
+  end
+end
+
+def display_result(result)
+  if result == "player"
+    prompt("You won!")
+  elsif result == "computer"
     prompt("Computer won!")
   else
     prompt("It's a tie!")
   end
 end
 
-loop do
-  choice = ''
-  valid_short_choices = %w(r p sc l sp)
-  loop do
-    prompt("Choose one: #{valid_short_choices.join(', ')}")
-    choice = translate_choice(Kernel.gets().chomp())
+def determine_grand_winner(player_score, computer_score)
+  if player_score >= 5 || computer_score >= 5
+    true
+  else
+    false
+  end
+end
 
-    if VALID_CHOICES.include?(choice)
+loop do
+  player_score = 0
+  computer_score = 0
+
+  loop do
+    choice = ''
+    valid_short_choices = %w(r p sc l sp)
+
+    loop do
+      prompt("Choose one: #{valid_short_choices.join(', ')}")
+      choice = translate_choice(Kernel.gets().chomp())
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample()
+
+    Kernel.puts("You chose #{choice}; Computer chose #{computer_choice}")
+
+    result = get_result(choice, computer_choice)
+    display_result(result)
+
+    player_score += 1 if result == 'player'
+    computer_score += 1 if result == 'computer'
+
+    grand_winner = determine_grand_winner(player_score, computer_score)
+
+    if grand_winner
+      prompt("Player is grand winner") if player_score == 5
+      prompt("Computer is grand winner") if computer_score == 5
       break
-    else
-      prompt("That's not a valid choice.")
     end
   end
-
-  computer_choice = VALID_CHOICES.sample()
-
-  Kernel.puts("You chose #{choice}; Computer chose #{computer_choice}")
-
-  display_result(choice, computer_choice)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
