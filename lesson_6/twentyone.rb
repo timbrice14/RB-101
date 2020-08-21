8e63dee812b15b8fe9d5fce4956358e18e293d3e
Tim Brice <tim.j.brice@gmail.com>
2020-08-21 11:09:41 -0500

    Refactor main game loop

--- lesson_6/twentyone.rb
+++ lesson_6/twentyone.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 WINNING_TOTAL = 21
 DEALER_STAY = 17
 VALID_CHOICES = %w(hit h stay s)
@@ -111,15 +113,92 @@ def display_hand(hand)
   "#{translate_card(hand[0])} of #{translate_suit(hand[1])}"
 end
 
+def starting_deck
+  cards = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
+  suits = %w(h s c d)
+  deck = cards.product(suits)
+  deck.shuffle!
+end
+
+def display_starting_hands(player_hand, dealer_hand)
+  prompt "Dealer has: #{display_hand(dealer_hand[1])} and unknown card"
+  prompt "You have: #{display_hand(player_hand[0])} and " \
+    "#{display_hand(player_hand[1])}"
+end
+
+def hit_or_stay
+  answer = ''
+  loop do
+    prompt "(h)it or (s)tay?"
+    answer = gets.chomp
+    break if VALID_CHOICES.include?(answer)
+    prompt "Please select a valid choice"
+  end
+
+  answer
+end
+
+def play_player_hand(hand, deck)
+  total = calculate_total(hand)
+  loop do
+    prompt "Player total is #{total}"
+
+    answer = hit_or_stay
+    break if answer == "stay" || answer == "s"
+
+    prompt "Player dealt #{display_hand(deck[0])}"
+    hand << deck.shift
+    total = calculate_total(hand)
+
+    if busted?(total)
+      prompt "Player busted!"
+      break
+    end
+  end
+
+  total
+end
+
+def display_dealer_starting_hand(hand, total)
+  prompt "Dealer flips up a #{display_hand(hand[0])} " \
+    "to go with the #{display_hand(hand[1])} " \
+    "for a total of #{total}"
+end
+
+def play_dealer_hand(hand, deck, player_total)
+  total = calculate_total(hand)
+  display_dealer_starting_hand(hand, total)
+
+  unless busted?(player_total)
+    until total >= DEALER_STAY
+      prompt "Dealer dealt #{display_hand(deck[0])}"
+      hand << deck.shift
+      total = calculate_total(hand)
+
+      if busted?(total)
+        prompt "Dealer busted!"
+        break
+      end
+    end
+  end
+
+  total
+end
+
+def overall_winner?(player_score, dealer_score)
+  player_score == 5 || dealer_score == 5
+end
+
+def announce_overall_winner(winner)
+  prompt "#{winner} wins the match!"
+end
+
 dealer_score = 0
 player_score = 0
 loop do
   system('clear')
   prompt welcome_message(player_score, dealer_score)
-  cards = %w(A 2 3 4 5 6 7 8 9 10 J Q K)
-  suits = %w(h s c d)
-  deck = cards.product(suits)
-  deck.shuffle!
+  deck = starting_deck
 
   first_player_card = deck.shift
   dealer_down_card = deck.shift
@@ -129,71 +208,20 @@ loop do
   player_hand = [first_player_card, second_player_card]
   dealer_hand = [dealer_down_card, dealer_up_card]
 
-  prompt "Dealer has: #{display_hand(dealer_up_card)} and unknown card"
-  prompt "You have: #{display_hand(first_player_card)} and " \
-    "#{display_hand(second_player_card)}"
+  display_starting_hands(player_hand, dealer_hand)
 
-  player_total = 0
-  loop do
-    player_total = calculate_total(player_hand)
-    prompt "Player total is #{player_total}"
+  player_total = play_player_hand(player_hand, deck)
+  dealer_total = play_dealer_hand(dealer_hand, deck, player_total)
 
-    answer = ''
-    loop do
-      prompt "(h)it or (s)tay?"
-      answer = gets.chomp
-      break if VALID_CHOICES.include?(answer)
-      prompt "Please select a valid choice"
-    end
-    break if answer == "stay" || answer == "s"
+  say_score(player_total, dealer_total)
+  say_winner(player_total, dealer_total)
+  winner = determine_winner(player_total, dealer_total)
+  player_score += 1 if winner == 'Player'
+  dealer_score += 1 if winner == 'Dealer'
 
-    prompt "Player dealt #{display_hand(deck[0])}"
-    player_hand << deck.shift
-    player_total = calculate_total(player_hand)
-
-    if busted?(player_total)
-      prompt "Player busted!"
-      break
-    end
-  end
-
-  dealer_total = calculate_total(dealer_hand)
-  prompt "Dealer flips up a #{display_hand(dealer_down_card)} of " \
-    "to go with the #{display_hand(dealer_up_card)} " \
-    "for a total of #{dealer_total}"
-
-  if busted?(player_total)
-    say_score(player_total, dealer_total)
-    say_winner(player_total, dealer_total)
-    dealer_score += 1
-  else
-    until dealer_total >= DEALER_STAY
-      prompt "Dealer dealt #{display_hand(deck[0])}"
-      dealer_hand << deck.shift
-      dealer_total = calculate_total(dealer_hand)
-
-      if busted?(dealer_total)
-        say_score(player_total, dealer_total)
-        say_winner(player_total, dealer_total)
-        player_score += 1
-        break
-      end
-    end
-  end
-
-  unless busted?(dealer_total) || busted?(player_total)
-    say_score(player_total, dealer_total)
-    say_winner(player_total, dealer_total)
-    winner = determine_winner(player_total, dealer_total)
-    player_score += 1 if winner == 'Player'
-    dealer_score += 1 if winner == 'Dealer'
-  end
-
-  if dealer_score == 5
-    prompt "Dealer wins the match!"
-    break
-  elsif player_score == 5
-    prompt "Player wins the match!"
+  if overall_winner?(player_score, dealer_score)
+    overall_winner = player_score == 5 ? 'Player' : 'Computer'
+    announce_overall_winner(overall_winner)
     break
   end
 
